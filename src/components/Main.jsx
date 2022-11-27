95f79e8db6b22e0232ae77bd57542f99859ffabf
othdr <otmax.vft@gmail.com>
2022-11-27 21:16:41 +0100

    font update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,20 +6,23 @@ const Main = () => {
 
   useEffect(() => {
     AOS.init({
-      duration : 1100
+      duration: 1100
     });
     AOS.refresh();
   }, []);
 
   return (
     <>
-      <div  name='Main' className='w-full h-screen flex flex-row justify-center px-2 py-6 bg-slate-900'>
+      <div name='Main' className='w-full h-screen flex flex-row justify-center px-2 py-6 bg-slate-900'>
         <div data-aos="zoom-in" className='flex flex-col justify-center md:items-start'>
           <h2 className='font-bold text-3xl md:text-5xl text-sky-500 px-2 text-center md:text-left'>
             About me
           </h2>
-          <p className='max-w-md md:max-w-3xl text-sm md:text-lg p-2 text-slate-500 text-center md:text-left'>I am a second year master student in the Information Systems Engineering, I code in many languages with diffrent purposes Web,Mobile,Desktop,DataScience ...,
-            actually I'm focused in the web so I build full-stack projects starting from the Client to the API.If you want a responsive dynamic/static website don't hesitate contacting me, I will realize your idea in the web.
+          <p className='max-w-md md:max-w-3xl text-sm md:text-lg p-2 text-slate-500 text-center md:text-left'>I am a second
+            year master student in the Information Systems Engineering, I code in many languages with diffrent purposes Web,
+            Mobile,Desktop,DataScience ...,
+            actually I'm focused in the web so I build full-stack projects starting from the Client to the API.If you want
+            a responsive dynamic/static website don't hesitate contacting me, I will realize your idea in the web.
           </p>
         </div>
       </div>
